013894e815c25f267a75dc0047d5bb344904d799
sagacity-monika-bandgar <124130019+sagacity-monika-bandgar@users.noreply.github.com>
2023-02-02 15:54:11 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,42 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  menuList = [
+    {
+      menu: 'Dashboard',
+      icon: 'dashboard',
+    },
+    {
+      menu: 'Hierarchy',
+      icon: 'account_tree',
+    },
+    {
+      menu: 'Template Engine',
+      icon: 'collections_bookmark',
+    },
+    {
+      menu: 'Prepare Data',
+      icon: 'file_copy',
+    },
+    {
+      menu: 'Upload',
+      icon: 'cloud_upload',
+    },
+    {
+      menu: 'Queue',
+      icon: 'radio_button_checked',
+    },
+    {
+      menu: 'User Management',
+      icon: 'person',
+    },
+    {
+      menu: 'Application Settings',
+      icon: 'settings',
+    },
+  ];
 }
