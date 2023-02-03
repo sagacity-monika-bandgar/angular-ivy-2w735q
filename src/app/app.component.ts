73096e134c944130698bff76f244b06146d484d3
sagacity-monika-bandgar <124130019+sagacity-monika-bandgar@users.noreply.github.com>
2023-02-03 13:40:52 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,5 +41,55 @@ export class AppComponent {
       icon: 'settings',
     },
   ];
-  panelOpenState = false;
+  accountData = [
+    {
+      name: 'Hudson Group',
+      count: 'C1245',
+      sacc: '10 S Account',
+      kacc: '530 K Accounts',
+      subscribers: '5125 subscribers',
+    },
+    {
+      name: 'Cosidine-Hammes',
+      count: 'C1345',
+      sacc: '123 S Account',
+      kacc: '252 K Accounts',
+      subscribers: '5208 subscribers',
+    },
+    {
+      name: 'Kuvalis,Swaniawski',
+      count: 'C1549',
+      sacc: '149 S Account',
+      kacc: '352 K Accounts',
+      subscribers: '7977 subscribers',
+    },
+    {
+      name: 'Erdman Inc',
+      count: 'C1845',
+      sacc: '150 S Account',
+      kacc: '936 K Accounts',
+      subscribers: '4586 subscribers',
+    },
+    {
+      name: 'Lehner-Herman',
+      count: 'C2223',
+      sacc: '129 S Account',
+      kacc: '542 K Accounts',
+      subscribers: '880 subscribers',
+    },
+    {
+      name: 'Stroman Group',
+      count: 'C1745',
+      sacc: '14 S Account',
+      kacc: '719 K Accounts',
+      subscribers: '6188 subscribers',
+    },
+    {
+      name: 'Herzog',
+      count: 'C2245',
+      sacc: '15 S Account',
+      kacc: '171 K Accounts',
+      subscribers: '181 subscribers',
+    },
+  ];
 }
